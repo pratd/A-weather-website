0fc9a09cd0315fd1639aaede1da6e3e0e93e91ba
pratd <39798358+pratd@users.noreply.github.com>
2019-11-13 14:33:58 +0100

    Update myjavascript-min.js

--- public/js/myjavascript-min.js
+++ public/js/myjavascript-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){city="moscow",$.ajax({url:"http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=c11fb4df334457589dd537b8b1dd9cbd",type:"GET",dataType:"json",success:function(t){console.log(t)}})});
+$(document).ready(function(){city="moscow",$.ajax({url:"http://api.openweathermap.org/data/2.5/weather?q="+city+"",type:"GET",dataType:"json",success:function(t){console.log(t)}})});
